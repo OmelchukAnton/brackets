abecb386e9bd20fc84a3aae863959ce38b19ee62
Anton Omelchuk <a2.omelchuk@itransition.com>
2021-08-30 13:34:01 +0300

    feat: completed brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketObj = new Object();
+    let openBracketsArr = [];
+    let stack = [];
+
+    for (let value of bracketsConfig) {
+        bracketObj[value[1]] = value[0];
+    }
+
+    for (let value of bracketsConfig) {
+        openBracketsArr.push(value[0]);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (openBracketsArr.includes(char)) {
+            bracketObj[char] === char && stack[stack.length - 1] === char ? stack.pop() : stack.push(char);
+        } else {
+            if (bracketObj[char] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 }
